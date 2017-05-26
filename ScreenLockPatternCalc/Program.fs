493ea21e69598b7ab934b27e7fcd71d19008c8b8
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2017-05-26 21:52:18 +0000

    Brute-force-ish ScreenLock Pattern count calculation implemented

--- ScreenLockPatternCalc/Program.fs
+++ ScreenLockPatternCalc/Program.fs
@@ -1,7 +1,90 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = int 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let rec gcd a b =
+    if b = 0 then
+        a
+    else gcd b (a%b)
+
+let subtractCoords (x1,y1) (x2,y2) =
+    (x1-x2,y1-y2)
+
+let addCoords (x1,y1) (x2,y2) =
+    (x1+x2,y1+y2)
+
+let divideCoords (x,y) d = (x/d,y/d)
+
+let multiplyCoords (x,y) m = (x*m,y*m)
+
+let CalculateLockPatternPermCount rows columns =
+    let toCoords ind =
+        ind / columns, ind % columns
+    let toIndex ((x,y) as coords) =
+        x*columns + y
+    let nodeCount = rows*columns
+    let indexLimit = nodeCount-1
+    let generator state = 
+        if state < nodeCount then
+            Some(state,state+1)
+        else 
+            None
+    let indices = Seq.unfold generator 0 |> Seq.toList
+    let coordsList = indices |> List.map toCoords
+
+    let expand a b =
+        let aC = toCoords a
+        let bC = toCoords b
+        let diff = subtractCoords bC aC
+        let diffGcd = diff ||> gcd |> abs
+        if diffGcd = 1 then
+            [a;b]
+        else
+//            printfn "%A" aC
+//            printfn "%A" bC
+//            printfn "%A" diff
+//            printfn "%A" diffGcd
+            let d = divideCoords diff diffGcd
+            seq {for i in 0..diffGcd -> addCoords aC (multiplyCoords d i)} |> Seq.map toIndex |> Seq.toList
+
+    let getFullPerm perm =
+        // Assume tail is full
+        match perm with
+        | a::[] -> perm
+        | [] -> perm
+        | a::b::tail -> List.append (expand a b) tail
+
+    let followingPerms perm = 
+        indices
+        |> List.except perm
+        |> List.map (
+            fun i ->
+                i::perm
+                |> getFullPerm
+                |> List.distinct
+            ) 
+
+    let rec perms ps =
+//        printfn "%A" ps
+        seq { for p in ps do
+                yield! followingPerms p |> perms
+                yield p
+        }
+
+    let allPerms = perms (indices |> List.map (fun i -> [i]))
+
+    printfn "%A" allPerms
+    
+    allPerms |> Seq.distinct |> Seq.length
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    match argv with
+    | [|Integer a; Integer b|] ->
+        CalculateLockPatternPermCount a b |> printfn "%A"
+        0
+    | _ -> failwith "arguments must be two integers"
